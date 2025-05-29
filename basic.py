244f67a73cc760be32a382771e91f0c5523f5983
Adiseshan <adiseshanramanan@.com>
2025-05-29 15:41:58 +0530

    added basic crud api logic as of now no dbs.

--- basic.py
+++ basic.py
@@ -9,18 +9,22 @@ class Book(BaseModel):
     description: str=Field(min_length=1, max_length=100)
     rating : float=Field(gt=-1.0, lt=10.0)
 
+#empty list of books
 Books=[]
 app=FastAPI()
+
 
 @app.get('/')
 async def start():
     return Books
 
+#creates books
 @app.post('/')
 async def createBook(book:Book):
     Books.append(book)
     return book
 
+#update those books in the Books list
 @app.put('/{bookID}')
 async def updateBook(bookID:UUID,book:Book):
     ctr=0
@@ -32,6 +36,7 @@ async def updateBook(bookID:UUID,book:Book):
     raise HTTPException(status_code=404, detail=f"book id {bookID} does not exist")
 
 
+#delete the books
 @app.delete('/{bookID}')
 async def deleteBook(bookID:UUID):
     ctr=0
