ddf36c8416692b90788fe146134b4e7d355e62d7
Adiseshan <adiseshanramanan@.com>
2025-05-29 15:22:49 +0530

    added basic code to create book

--- basic.py
+++ basic.py
@@ -1,9 +1,23 @@
 from fastapi import FastAPI
+from pydantic import BaseModel, Field 
+from uuid import UUID
+
+class Book(BaseModel):
+    id: UUID
+    title: str=Field(min_length=1,max_length=100)
+    author: str=Field(min_length=1,max_length=100)
+    description: str=Field(min_length=1, max_length=100)
+    rating : float=Field(gt=-1.0, lt=10.0)
+
+Books=[]
+
+
 app=FastAPI()
 @app.get('/')
 async def hello():
-    return {"message":"hello!"}
+    return Books
 
-@app.get('/{name}')
-async def greet(name:str) -> dict:
-    return {"message":f"hello, {name}"}
+@app.post('/')
+async def createBook(book:Book):
+    Books.append(book)
+    return book
